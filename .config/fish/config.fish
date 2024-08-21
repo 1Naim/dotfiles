1a8d96a211c40590c1e9e1864ecf3d7a196af024
Eric Naim <dnaim@cachyos.org>
2024-08-21 16:11:01 +0700

    21-08-2024

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,8 +11,10 @@ alias pacin='aura -S'
 alias pacrm='aura -Rns'
 alias aurin='aura -A'
 alias edit-scx='command sudo vim /etc/default/scx && command sudo systemctl restart scx'
-alias pacsrh='pacman -Ss'
-alias pacls='pacman -Qs'
+alias pacsrh="pacman -Slq | fzf --preview 'pacman -Si {}' --layout=reverse --bind 'enter:execute(pacman -Si {} | less)'"
+alias pacview="pacman -Qq | fzf --preview 'pacman -Qil {}' --layout=reverse --bind 'enter:execute(pacman -Qil {} | less)'"
+alias pacup='aura -Syu'
+alias aurup='aura -Ayu'
 alias aursrh='aura -As'
 alias aurls='pacman -Qm'
 alias spotx='bash <(curl -sSL https://spotx-official.github.io/run.sh)'
@@ -26,9 +28,19 @@ alias onac='echo 1 | run0 --background=0 tee /sys/bus/platform/drivers/ideapad_a
 #set --global tide_context_always_display 'true'
 #set --global tide_context_hostname_parts '0'
 set --global tide_git_icon ''
+set --global async_prompt_functions fish_prompt
+
+# Zellij
+set -g ZELLIJ_AUTO_ATTACH true
+#set -g ZELLIJ_AUTO_EXIT true
 
 source /usr/share/doc/pkgfile/command-not-found.fish
 
 eval "$(zoxide init fish)"
 starship init fish | source
 enable_transience
+
+if status is-interactive
+    eval (zellij setup --generate-auto-start fish | string collect)
+end
+
